5c1541ee83b09e92fee8d4017c9e56c1434b0d72
ferya2 <adefery221@gmail.com>
2024-02-04 01:43:43 +0700

    penyesuaian pada delivery page

--- app_rest/app_rest/lib/view/delivery.dart
+++ app_rest/app_rest/lib/view/delivery.dart
@@ -2,6 +2,7 @@
 
 import 'package:app_rest/constanst/colors/colors.dart';
 import 'package:app_rest/constanst/textstyles/textstyle.dart';
+import 'package:app_rest/view/home.dart';
 import 'package:flutter/material.dart';
 
 class DeliveryPage extends StatefulWidget {
@@ -22,8 +23,10 @@ class _DeliveryPageState extends State<DeliveryPage> {
         leading: Padding(
           padding: const EdgeInsets.only(left: 20),
           child: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.arrow_back_ios_new_outlined),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back_ios_new_outlined),
             iconSize: 20,
           ),
         ),
@@ -50,8 +53,147 @@ class _DeliveryPageState extends State<DeliveryPage> {
                   'change',
                   style: AppTextStyle.changeDelivery,
                 )
-              ])
+              ]),
+              const SizedBox(
+                height: 16,
+              ),
+              Container(
+                height: 180,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: AppColor.whiteNeutral,
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(20.0),
+                    )),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 20, right: 20, top: 20, bottom: 20),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Marvis Kprabo',
+                            style: AppTextStyle.appBarTextStyle),
+                        Divider(
+                          color: AppColor.greyNeutral,
+                          thickness: 0.5,
+                          height: 10,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                            'Km 5 refinery road oppsite republic road, effurun, delta state',
+                            overflow: TextOverflow.clip,
+                            style: AppTextStyle.appBarTextStyle2),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Divider(
+                          color: AppColor.greyNeutral,
+                          thickness: 0.5,
+                          height: 10,
+                        ),
+                        Text('+234 9011039271',
+                            style: AppTextStyle.appBarTextStyle2),
+                      ]),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Delivery Method',
+                style: AppTextStyle.appBarTextStyle,
+              ),
+              const SizedBox(height: 16),
+              Container(
+                height: 180,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: AppColor.whiteNeutral.withOpacity(0.8),
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(16.0),
+                    )),
+                child: ListView(
+                  padding: const EdgeInsets.only(top: 20, bottom: 20),
+                  children: [
+                    ListTile(
+                        leading: Radio(
+                          value: 1,
+                          groupValue: 1,
+                          onChanged: (value) {},
+                        ),
+                        title: Row(
+                          children: [
+                            const SizedBox(width: 20),
+                            Text(
+                              'Door delivery',
+                              style: AppTextStyle.appBarTextStyle2,
+                            ),
+                          ],
+                        )),
+                    const Divider(
+                      thickness: 1,
+                      indent: 70,
+                      endIndent: 25,
+                    ),
+                    ListTile(
+                        leading: Radio(
+                          value: 2,
+                          groupValue: 1,
+                          onChanged: (value) {},
+                        ),
+                        title: Row(
+                          children: [
+                            const SizedBox(width: 20),
+                            Text(
+                              'Pick up',
+                              style: AppTextStyle.appBarTextStyle2,
+                            ),
+                          ],
+                        )),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 88),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Total',
+                    style: AppTextStyle.appBarTextStyle2,
+                  ),
+                  Text(
+                    '23,000',
+                    style: AppTextStyle.nameTittleCenter,
+                  )
+                ],
+              )
             ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+        child: SizedBox(
+          height: 65,
+          width: double.infinity,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              backgroundColor: AppColor.primaryColor,
+            ),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return const HomePage();
+              }));
+            },
+            child: Text(
+              'Proceed to Payment',
+              style: AppTextStyle.btnSign,
+            ),
           ),
         ),
       ),
